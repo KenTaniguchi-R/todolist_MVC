ba9d993b1f5fdaf5e4e38906cc5689977f54b8e4
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2023-03-10 12:16:43 -0800

    fixed duplicated ids

--- index.js
+++ index.js
@@ -136,18 +136,18 @@ const View = (() => {
         todos.forEach((todo) => {
             const liTemplate = `
             <li>
-                ${todo.completed ? `<button class="complete-btn" completed id="${todo.id}">
+                ${todo.completed ? `<button class="complete-btn" completed data-id="${todo.id}">
                     <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path></svg>
                 </button>` : ""}
                 <span class="content">${todo.content}</span>
-                <input type="text" class="edit-input" id="${todo.id}" value="${todo.content}" style="display: none;">
-                <button class="edit-btn" id="${todo.id}">
+                <input type="text" class="edit-input" data-id="${todo.id}" value="${todo.content}" style="display: none;">
+                <button class="edit-btn" data-id="${todo.id}">
                     <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24"  aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
                 </button>
-                <button class="delete-btn" id="${todo.id}">
+                <button class="delete-btn" data-id="${todo.id}">
                     <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
                 </button>
-                ${todo.completed ? "" : `<button class="complete-btn" pending id="${todo.id}">
+                ${todo.completed ? "" : `<button class="complete-btn" pending data-id="${todo.id}">
                     <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="ArrowForwardIcon" aria-label="fontSize small"><path d="m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path></svg>
                 </button>`}
             </li>`;
@@ -212,8 +212,7 @@ const Controller = ((view, model) => {
 
         function deleteTodo(event){
             if (event.target.className === "delete-btn") {
-                const id = event.target.id;
-                console.log("id", typeof id);
+                const id = event.target.getAttribute("data-id");
                 model.deleteTodo(+id).then((data) => {
                     state.todos = state.todos.filter((todo) => todo.id !== +id);
                 });
@@ -230,7 +229,7 @@ const Controller = ((view, model) => {
                 const content = event.target.parentElement.querySelector(".content");
                 const editInput = event.target.parentElement.querySelector(".edit-input");
                 if (event.target.hasAttribute("editing")) {
-                    const id = event.target.id;
+                    const id = event.target.getAttribute("data-id");
                     model.updateTodo(+id, {content: editInput.value}).then((data) => {
                         state.todos = state.todos.filter((todo) => {
                             if (todo.id === +id) {
@@ -257,7 +256,7 @@ const Controller = ((view, model) => {
 
         function updateComplete(event) {
             if (event.target.className === "complete-btn") {
-                const id = event.target.id;
+                const id = event.target.getAttribute("data-id");
 
                 model.updateTodo(+id, {completed: !event.target.hasAttribute("completed")}).then((data) => {
                     state.todos = state.todos.filter((todo) => {
